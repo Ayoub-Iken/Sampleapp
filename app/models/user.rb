46254623b674c0529ad87351a00fd0f9cba99577
ayoubiken <l.ayoubiken@gmail.com>
2015-10-26 21:11:15 +0000

    Adding the User model with password columns

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+before_save { self.email = email.downcase }
+
+VALID_EMAIL = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {maximum: 20}
+
+  validates :email, presence: true, length: {maximum: 255},
+                    format: { with: VALID_EMAIL},
+                    uniqueness: { case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {maximum: 8}
+end
