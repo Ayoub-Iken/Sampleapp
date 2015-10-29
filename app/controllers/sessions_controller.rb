b86b074aa430486843206aae7c3aedaf1b97271e
ayoubiken <l.ayoubiken@gmail.com>
2015-10-29 11:24:23 +0000

    fixing the login issue, by fixing the markup html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,8 @@ def create
    if user && user.authenticate(params[:session][:password])
     #Log the user in and redirect to the user's show page.
   else
-    flash[:danger] = "Invalid email/password combination"
-      render :action =>  'new'
+    flash.now[:danger] = "Invalid email/password combination"
+     render :action =>  'new'
   end
 end
 
