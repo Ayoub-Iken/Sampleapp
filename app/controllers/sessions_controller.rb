2ecd8ccfa8de9efe5dc1db7baf2884d3fbf7f291
ayoubiken <l.ayoubiken@gmail.com>
2015-10-30 16:40:42 +0000

    Finalizing logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,9 @@ class SessionsController < ApplicationController
 def create
   user = User.find_by(email: params[:session][:email])
    if user && user.authenticate(params[:session][:password])
+     log_in user
+     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+     redirect_to user
     #Log the user in and redirect to the user's show page.
   else
     flash.now[:danger] = "Invalid email/password combination"
@@ -18,7 +21,8 @@ end
 
 # ***********Destroy action
   def destroy
-
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 end
